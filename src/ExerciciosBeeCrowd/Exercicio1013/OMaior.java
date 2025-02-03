bbb2bae8f2095784edb637528979f7add8e79d59
MaiconDev <haelyton149@gmail.com>
2025-02-02 21:13:33 -0300

    Exercicio - (1013)

---
+++ src/ExerciciosBeeCrowd/Exercicio1013/OMaior.java
@@ -0,0 +1,29 @@
+package ExerciciosBeeCrowd.Exercicio1013;
+
+import java.util.Scanner;
+
+public class OMaior {
+
+    public static Integer MaiorAb(int A, int B) {
+        return (A + B + (A - B)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+        int maiorA = MaiorAb(A, B);
+        int maiorB = MaiorAb(B, A);
+
+        if (maiorA > C && maiorA > maiorB){
+            System.out.printf(maiorA + " eh o maior\n");
+        }else if (maiorB > C){
+            System.out.printf(maiorB + " eh o maior\n");
+        }else {
+            System.out.printf(C + " eh o maior\n");
+        }
+    }
+}
